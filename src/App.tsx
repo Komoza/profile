0669c6df67141f46782d346541d84d164dc32f44
komoza <MaximKomoza@gmail.com>
2025-01-17 19:28:09 +0700

    feat: add preloader

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import { AppRoutes } from './routes/routes.tsx';
 import './global.sass';
 import { setTheme } from './themes/setup.ts';
 import { useSingleEffect } from './hooks/singleEffect.tsx';
+import { useState } from 'react';
+import { Preloader } from './components/preloader/preloader.tsx';
 
 export const App = () => {
+  const [isShowPreloader, setIsShowPreloader] = useState(false);
+
+  const showPreloader = () => {
+    setIsShowPreloader(true);
+    setTimeout(() => {
+      setIsShowPreloader(false);
+    }, 4000);
+  };
+
   useSingleEffect(() => {
+    showPreloader();
     setTheme();
   }, []);
 
   return (
     <div className='container'>
+      {isShowPreloader && <Preloader />}
       <Sidebar />
       <AppRoutes />
     </div>
---
+++ src/components/preloader/preloader.tsx
@@ -0,0 +1,7 @@
+export const Preloader = () => {
+  return (
+    <h1 className='preloader'>
+      <div className='preloader__loader'></div>
+    </h1>
+  );
+};
